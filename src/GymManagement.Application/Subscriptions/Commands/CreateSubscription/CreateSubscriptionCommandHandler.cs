56d04fee29c9eeb15fc1534ea56eed818b517100
Alexei Pancratov <drivergear@gmail.com>
2023-12-16 13:24:13 -0500

    Added validation for CreateSubscription

--- src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -7,14 +7,28 @@ namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 
 public class CreateSubscriptionCommandHandler(
     ISubscriptionsRepository subscriptionsRepository,
+    IAdminsRepository adminsRepository,
     IUnitOfWork unitOfWork)
     : IRequestHandler<CreateSubscriptionCommand, Result<Subscription>>
 {
     private readonly ISubscriptionsRepository _subscriptionsRepository = subscriptionsRepository;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IAdminsRepository _adminsRepository = adminsRepository;
 
     public async Task<Result<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
     {
+        var admin = await _adminsRepository.GetByIdAsync(request.AdminId);
+
+        if (admin is null)
+        {
+            return Result.NotFound("Admin not found");
+        }
+        
+        if (admin.SubscriptionId is not null)
+        {
+            return Result.Conflict("Admin already has a subscription");
+        }
+        
         var subscription = new Subscription(
             subscriptionType: request.SubscriptionType,
             adminId: request.AdminId);
