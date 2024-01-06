e30bfdcb47001e456d46222ddc8487006c68ac4f
Alexei Pancratov <drivergear@gmail.com>
2024-01-06 12:47:43 -0500

    Fixed subcutaneous test (due to invalid AdminId)

--- tests/TestCommon/TestConstants/Constants.Admin.cs
+++ tests/TestCommon/TestConstants/Constants.Admin.cs
@@ -4,6 +4,6 @@ public static partial class Constants
 {
     public static class Admin
     {
-        public static Guid Id = Guid.NewGuid();
+        public static Guid Id = new("2150e333-8fdc-42a3-9474-1a3956d46de8");
     }
 }
