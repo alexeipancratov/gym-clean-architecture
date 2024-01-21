78aff15f7903f5e004459959f087b86a1e0efed2
Alexei Pancratov <drivergear@gmail.com>
2024-01-21 13:23:01 -0500

    Added HTTP request files for Authentication/Profiles

--- src/GymManagement.Domain/Common/Interfaces/IPasswordHasher.cs
+++ src/GymManagement.Domain/Common/Interfaces/IPasswordHasher.cs
@@ -3,6 +3,10 @@ using GymManagement.Core.ErrorHandling;
 
 namespace GymManagement.Domain.Common.Interfaces;
 
+/// <summary>
+/// Provides methods for hashing and verifying passwords.
+/// </summary>
+/// <remarks>It lives in the Domain Layer because it's being used by domain objects.</remarks>
 public interface IPasswordHasher
 {
     public Result<string, OperationError> HashPassword(string password);
